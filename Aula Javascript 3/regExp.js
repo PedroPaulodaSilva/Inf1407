515b3b64e3226bce987fefe96389074a6dcd8b1c
PedroPaulodaSilva <pedro.ppds@gmail.com>
2017-10-16 19:54:52 -0200

    Update regExp.js

--- Aula Javascript 3/regExp.js
+++ Aula Javascript 3/regExp.js
@@ -11,6 +11,8 @@ function verifica(){
 	var resultado = document.getElementById("resultado");
 	var campo = document.getElementById("teste").value;
 	var re = /\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}/;
+	//var re = /^\d{4,5}\-\d{4}$/; expressão para testar celulares com 8 ou 9 digitos
+	//var re = /^9\d{4}\-\d{4}\$/; expressão para testar celular começando com 9
 	if(re.test(campo)){
 		resultado.innerHTML = "OK";
 	}
