eb31fd963908252a5a307f9012e2762d0eef0c80
PedroPaulodaSilva <pedro.ppds@gmail.com>
2017-10-16 20:25:34 -0200

    Create regExp-email.js

---
+++ Aula Javascript 3/regExp-email.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+onload = inicia;
+
+function inicia(){
+	document.getElementById("botao").addEventListener("click", separa);
+}
+
+function separa(){
+	var email = document.getElementById("email").value;
+	//var re = /(.+)@(.+)/; pra receber qualquer letra + @ + qualquer letra
+	//var re = /(([a-z]+)@)([a-z]+)/; receber email que contenha apenas letras, no numbers allowed
+	var re = /^[a-z\-]+@[a-z\-]+(\.[a-z]+)+/;
+	var resultado = re.exec(email);
+	console.log("0: " + resultado[0]);
+	console.log("1: " + resultado[1]);
+	console.log("2: " + resultado[2]);
+	
+	document.getElementById('usuario').innerHTML = resultado[1];
+	document.getElementById('dominio').innerHTML = resultado[2];
+}
